6cced9fac42de0fe00fc9bf6c5d84e3f3a6ef265
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-09-27 06:12:24 -0400

    added method for converting temperature

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,4 +1,4 @@
-# method for inputing temperature
+
 def temperature_converter():
     #  method for choosing the convertion type
     while True:
@@ -10,8 +10,15 @@ def temperature_converter():
             break
         else:
             print("Invalid. Please input 1 or 2.")
-    
+    #for inputing temperature
     temp = float(input("Input temperature: "))
+
+    if choice == 1:
+        converted_temp = (temp * 9/5) + 32
+        print(f"{temp} °C is equal to {converted_temp} °F")
+    elif choice == 2:
+        converted_temp = (temp - 32) * 5/9
+        print(f"{temp} °F is equal to {converted_temp} °C")
 
 temperature_converter()
 # method for selecting the convertion type
