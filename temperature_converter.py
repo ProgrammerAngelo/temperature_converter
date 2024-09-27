e86ff92144d231fc343a19af834aa2727a2ef4e9
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-09-27 06:01:39 -0400

    added method for choosing the the method of conversion

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,2 +1,15 @@
 # method for inputing temperature
+def temperature_converter():
+    #  method for choosing the convertion type
+    while True:
+        print("Select a convertion type: \nInput 1 for Celcius to farenhiet \nInput 2 for farenheit to celcius")
+        #for getting the user choice
+        choice = int(input("Input number 1 or 2: "))
+        #for asking to input again when the user input numbers beside 1 and 2
+        if choice in [1,2]:
+            break
+        else:
+            print("Invalid. Please input 1 or 2.")
+
+temperature_converter()
 # method for selecting the convertion type
